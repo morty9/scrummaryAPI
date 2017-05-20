55eeb4b61983c3d0016a9ccea60263a70bd98c55
berangere.la.touche <berangere.lt@gmail.com>
2017-05-20 16:13:57 +0200

    connection database failed

--- models/index.js
+++ models/index.js
@@ -3,16 +3,26 @@ const Sequelize = require('sequelize');
 module.exports = (api) => {
 	console.log('initializing models...');
 	console.log('SETTINGS', api.settings);
-	api.sequelize = new Sequelize (
-		api.settings.db.sql.user,
-		api.settings.db.sql.password,
-		api.settings.db.sql.database, (
-			api.settings.db.sql.host,
-			api.settings.db.sql.dialect,
-			api.settings.db.sql.pool
-		)
-	);
-	api.sequelize.sync();
+	// api.sequelize = new Sequelize (
+	// 	api.settings.db.sql.database,
+	// 	api.settings.db.sql.user,
+	// 	api.settings.db.sql.password, {
+	// 		host: api.settings.db.sql.host,
+	// 		port : api.settings.db.sql.port,
+	// 		dialect: api.settings.db.sql.dialect
+	// 	}
+	// );
+
+	api.sequelize = new Sequelize(
+		'scrummary',
+		'adminScrummary',
+		'admin', {
+			host : 'localhost',
+			port : 8889,
+			dialect : 'mysql'
+		}
+	)
+
 	api.models = {
 		User: require('./User')(api)
 		//Task: require('./Task')(api),
