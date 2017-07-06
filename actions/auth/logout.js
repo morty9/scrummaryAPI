c67a28e936923b043d26f023af9a0bd86e7c7818
Zeke <hnam.nguyen1708@gmail.com>
2017-07-06 12:58:08 +0200

    auth

--- actions/auth/logout.js
+++ actions/auth/logout.js
@@ -1,3 +1,11 @@
 module.exports = (api) => {
-  
-}
+  const Token = api.models.Token;
+
+  return function logout(req, res, next) {
+    Token.remove({userId: req.auth.userId}, (err, data) => {
+            if (err)
+                return res.status(500).send(err);
+            res.send('logout.succeeded');
+        });
+  }
+};
