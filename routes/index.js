a8a1da441a8f3745f02f30002a0bb8807d191c40
Zeke <hnam.nguyen1708@gmail.com>
2017-07-06 11:00:16 +0200

    authentification ok (to test)

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 module.exports = (api) => {
 	console.log('initializing routes...');
 	api.use(api.middlewares.logger);
+	api.use('/scrummary/auth', require('./auth')(api));
 	api.use('/scrummary/users', require('./users')(api));
 	api.use('/scrummary/projects', require('./projects')(api));
 	api.use('/scrummary/sprints', require('./sprints')(api));
